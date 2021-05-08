f7c2b41e0493029ed9f97999591f084e25101fc4
Crazy-Newbie <55473294+Crazy-Newbie@users.noreply.github.com>
2021-05-08 21:32:23 +0800

    Update soal2a.c

--- Soal2/soal2a.c
+++ Soal2/soal2a.c
@@ -1,45 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main(){
+int mat1[10][10];
+int mat2[10][10];
+int mat3[10][10];
+int row1 = 4, col1= 3, row2 = 3, col2 = 6;
+pthread_t tid[10];
 
-   int mat1[10][10];
-   int mat2[10][10];
-   int mat3[10][10];
-   int row1 = 4, col1= 3, row2 = 3, col2 = 6;
-   int i,j,k;
-
-   //Matriks 4 x 3
-   for(i=0; i<row1 ; i++){
+//Matriks 4 x 3
+void *num_mat1(void *arg){
+   int i,j;
+   for(i=0; i<row1; i++){
       for(j=0; j<col1; j++){
-         scanf("%d", &mat1[i][j]);
+         scanf("%d", &mat1[i][j])
       }
    }
-   
-   
-   //Matriks 3 x 6
-   for(i=0; i<row2; i++){
+}
+
+//Matriks 3 x 6
+void *num_mat2(void *arg){
+   int i,j;
+   for(i=0; i<row1; i++){
       for(j=0; j<col2; j++){
          scanf("%d", &mat2[i][j]);
       }
    }
+}
 
-   //Perkalian Matriks
+//Perkalian
+void *calculate(void *arg){
+   int i,j,k;
    for(i=0; i<row1; i++){
       for(j=0; j<col2; j++){
-         for(k=0; k<col1; k++){
+         for(k=0; k<col2; k++){
             mat3[i][j] = mat1[i][k] * mat2[k][j];
          }
       }
    }
-   
-   //Print Hasil
-   for(i=0; i<row1; i++){
+}
+
+//Print Hasil
+void *print_result(void *arg){
+   int i,j;
+   for(i=0; i<ro21; i++){
       for(j=0; j<col2; j++){
-         printf("[%d] ", mat3[i][j]);
-         if(j==col2-1){
-            printf("\n");
-         }
+         printf("[%d]\t", &mat3[i][j]);
       }
    }
+}
+
+int main(){
+
+   pthread_create(&tid[0],NULL,num_mat1,NULL);
+   pthread_create(&tid[1],NULL,num_mat2,NULL);
+
+   pthread_join(tid[0],NULL);
+   pthread_join(tid[1],NULL);
+   
+   pthread_create(&tid[2],NULL,calculate,NULL);
+   pthread_join(tid[2],NULL);
+
+   pthread_create(&tid[3],NULL,print_result,NULL);
+   pthread_join(tid[3],NULL);
+   
 
 }
